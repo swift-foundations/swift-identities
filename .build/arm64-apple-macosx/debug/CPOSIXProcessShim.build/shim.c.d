8af9466d977dde11c045d60ee9e6b8f2c93004e1
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-04-09 15:13:00 +0200

    Save progress: 2026-04-09

--- .build/arm64-apple-macosx/debug/CPOSIXProcessShim.build/shim.c.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/coen/Developer/swift-standards/swift-iso-9945/Sources/CPOSIXProcessShim/include/module.modulemap \
-  /Users/coen/Developer/swift-standards/swift-iso-9945/Sources/CPOSIXProcessShim/shim.c
